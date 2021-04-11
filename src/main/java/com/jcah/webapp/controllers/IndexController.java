e7427976c4f934cdbe5369cf06ac4b79d6098f6c
Juls <jcah549@gmail.com>
2021-04-11 00:22:28 -0500

    primer commit del proyecto

---
+++ src/main/java/com/jcah/webapp/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.jcah.webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping({"/index","/","/home"})
+	public String index(Model model) {
+		
+		model.addAttribute("titulo", "Hola spring framework");
+		
+
+		return "index";
+	}
+
+}
